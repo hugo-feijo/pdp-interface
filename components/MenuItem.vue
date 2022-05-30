46b1b402e6c529411868674e0569cb8db8be52dc
Hugo Feijo <hugo.ofeijo@gmail.com>
2022-05-30 19:49:00 -0300

    fix: moving modal to MenuItem

--- components/MenuItem.vue
+++ components/MenuItem.vue
@@ -1,21 +1,60 @@
 <template>
-  <div class="flex block-item h-8rem px-3 mb-4">
-    <div class="flex justify-content-center image-item overflow-hidden w-6" @click="$emit('itemSelected', item)">
-      <img class="h-full" :src="item.image" >
-    </div>
-    <ScrollPanel style="width: 100%; height: 100%">
-      <p class="mt-0 font-semibold text-xl">{{item.name}}</p>
-      <p>{{item.description}}</p>
-    </ScrollPanel>
+  <div>
+    <div class="flex block-item h-8rem px-3 mb-4">
+      <div class="flex justify-content-center image-item overflow-hidden w-6" @click="itemSelected(item)">
+        <img class="h-full" :src="item.image" >
+      </div>
+      <ScrollPanel style="width: 100%; height: 100%">
+        <p class="mt-0 font-semibold text-xl">{{item.name}}</p>
+        <p>{{item.description}}</p>
+      </ScrollPanel>
+    </div>      
+    <Dialog 
+      :show-header="false" 
+      v-model:visible="showModal" 
+      modal 
+      dismissableMask 
+      contentClass="item-modal"
+      :style="{'max-width': '600px'}">
+      
+      <div class="header-modal flex justify-content-center overflow-hidden h-10rem">
+        <img class="image-modal" :src="selectedItem.image" >
+      </div>
+      <div class="content">
+        <p>{{selectedItem.description}}</p>
+      </div>
+      <template #footer>
+        <PeopleHeader />
+      </template>
+    </Dialog>
   </div>
 </template>
 <script setup lang="ts">
 
+interface Item {
+  name: string;
+  description: string;
+  image: string;
+}
 const props = defineProps({
   item: {
     type: Object
   }
 })
+
+const showModal = ref(false)
+
+
+const selectedItem = ref<Item>({
+  name: '',
+  description: '',
+  image: '',
+})
+
+function itemSelected(item) {
+  selectedItem.value = item
+  showModal.value = true
+}
 </script>
 
 <style lang="scss" scoped>
@@ -25,5 +64,15 @@ const props = defineProps({
 
 .image-item {
   border-radius: 0.75rem;
+}
+
+.image-modal {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.content{
+  padding: 0 1.5rem 2rem;
 }
 </style>
--- components/MenuSection.vue
+++ components/MenuSection.vue
@@ -3,39 +3,14 @@
     <h2 class="ml-4 text-2xl font-bold">{{config.section}}</h2>
     <ClientOnly>
       <div :key="item.id" v-for="item in config.items">
-        <MenuItem :item="item" @item-selected="itemSelected"/>
+        <MenuItem :item="item"/>
       </div>
-      
-    <Dialog 
-      :show-header="false" 
-      v-model:visible="showModal" 
-      modal 
-      dismissableMask 
-      contentClass="item-modal"
-      :style="{'max-width': '600px'}">
-      
-      <div class="header-modal flex justify-content-center overflow-hidden h-10rem">
-        <img class="image-modal" :src="selectedItem.image" >
-      </div>
-      <div class="content">
-        <p>{{selectedItem.description}}</p>
-      </div>
-      <template #footer>
-          <Button label="No" icon="pi pi-times" @click="showModal = !showModal" class="p-button-text"/>
-          <Button label="Yes" icon="pi pi-check" @click="showModal = !showModal" autofocus />
-      </template>
-    </Dialog>
     </ClientOnly>
   </div>
 </template>
 
 <script setup lang="ts">
 
-interface Item {
-  name: string;
-  description: string;
-  image: string;
-}
 const props = defineProps({
   config: {
     type: Object,
@@ -43,18 +18,6 @@ const props = defineProps({
   },
 })
 
-const showModal = ref(false)
-
-const selectedItem = ref<Item>({
-  name: '',
-  description: '',
-  image: '',
-})
-
-function itemSelected(item) {
-  selectedItem.value = item
-  showModal.value = true
-}
 </script>
 
 <style lang="scss" scoped>
@@ -62,15 +25,5 @@ function itemSelected(item) {
   border-radius: 0.75rem;
   background-color: #fff;
   max-width: 600px;
-}
-
-.image-modal {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
-
-.content{
-  padding: 0 1.5rem 2rem;
 }
 </style>
