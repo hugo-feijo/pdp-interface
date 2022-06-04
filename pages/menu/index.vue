bb91e28a0db7ca646a2d71ed557bf8da09591786
Hugo Feijo <hugo.ofeijo@gmail.com>
2022-06-04 13:40:17 -0300

    feat: adding card and home button

--- pages/menu/index.vue
+++ pages/menu/index.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="pt-2 flex flex-column align-items-center flex-wrap" style="gap: 2rem">
-    <span class="p-input-icon-right p-float-label">
-      <InputText id="search" type="text" v-model="searchText"/>
-      <label for="search">Pesquisar</label>
-      <i class="pi pi-search"/>
-    </span>
+    <div class="flex w-full">
+      <div class="flex-none flex align-items-center justify-content-center">
+        <Button class="p-button-rounded p-button-secondary p-button-text p-button-lg" icon="pi pi-home"/>
+      </div>
+      <div class="flex-grow-1 flex align-items-center justify-content-center">
+        <span class="p-input-icon-right p-float-label">
+          <InputText id="search" type="text" v-model="searchText"/>
+          <label for="search">Pesquisar</label>
+          <i class="pi pi-search"/>
+        </span>
+      </div>
+      <div class="flex-none flex align-items-center justify-content-center">
+        <Button class="p-button-rounded p-button-secondary p-button-text p-button-lg" icon="pi pi-shopping-cart"/>
+      </div>
+    </div>
     <MenuSection :config="foods" class="py-2"/>
     <MenuSection :config="drinks" class="py-2"/>
   </div>
