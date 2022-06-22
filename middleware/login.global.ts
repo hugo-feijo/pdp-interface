2801c2c79cfaac71951121a34be99dc2e13d50f2
Hugo Feijo <hugo.ofeijo@gmail.com>
2022-06-22 11:17:16 -0300

    feat: moving loging client to menu

--- middleware/login.global.ts
+++ middleware/login.global.ts
@@ -1,9 +1,13 @@
 import { useStore } from "@/stores/main-store"
 
-const whiteList = ['index', 'table-id']
+const whiteListHome = ['index', 'table-id']
+const blackListLogin = ['menu', 'checkout']
 
 export default defineNuxtRouteMiddleware((to, from) => {
   const store = useStore()
-  if(store.currentClient.id == 0 && !whiteList.includes(to.name.toString()))
+  if(store.currentClient.id == 0 && !whiteListHome.includes(to.name.toString()))
     return navigateTo('/')
+
+  if(store.currentClient.id != 0 && !blackListLogin.includes(to.name.toString()))
+    return navigateTo('/menu')
 })
