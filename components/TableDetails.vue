c1e304612af98ab348c0cee12dbb53a0449e010f
Hugo Feijó <hugo.ofeijo@gmail.com>
2023-04-20 16:45:00 -0300

    feat: show hours

--- components/TableDetails.vue
+++ components/TableDetails.vue
@@ -53,7 +53,7 @@ function afterHide() {
 }
 
 function formatBrDate(date: string) {
-  return new Date(date).toLocaleDateString('en-GB', {day: '2-digit', month: '2-digit', year: 'numeric'});
+  return new Date(date).toLocaleDateString('en-GB', {day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit'});
 }
 
 async function inactiveClient(clientId: number, orderPadId: number) {
